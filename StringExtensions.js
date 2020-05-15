f6c4bbcc3d675f800d00ab7e33b2750fdbd9685e
Elizabeth Samatepo <elizabeth.samatepo@tafensw.edu.au>
2020-05-15 12:53:31 +1000

    string extensions lesson

---
+++ StringExtensions.js
@@ -0,0 +1,19 @@
+let sentence = "The quick brown fox jumps over the lazy dog.";
+
+/*String.includes
+let hasDog = sentence.includes('dog');
+console.log(hasDog);
+console.log(sentence.toLowerCase().includes("z"));
+
+
+String.indexOf
+
+let dogIndex = sentence.indexOf("dog");
+console.log(dogIndex);
+let invalidIn
+
+
+ String.split */
+
+let myWords = sentence.split(" ");
+console.log(myWords);
